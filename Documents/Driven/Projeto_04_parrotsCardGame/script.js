29fa497fcc1b0f58878c11b0f76e4a897a622507
KlausDk <klausdk1999@gmail.com>
2022-04-05 22:37:07 +0000

    Criando cartas no html

--- Documents/Driven/Projeto_04_parrotsCardGame/script.js
+++ Documents/Driven/Projeto_04_parrotsCardGame/script.js
@@ -3,13 +3,14 @@ let positions=[];//criar array para cartas que serao embaralhadas
 
 nCards();
 defineCards();
+createCards();
 //gera o prompt ate que se obtenha valor valido
 function nCards(){
     while(ncards!=4 && ncards!=6 && ncards!=8 && ncards!=10 && ncards!=12 && ncards!=14){
         ncards=Number(prompt("Com quantas cartas deseja jogar? (4, 6, 8, 10, 12, 14)"));
     }
 }
-
+//define array com o numero escolhido de cards e mistura
 function defineCards(){
     for(let i=0;i<(ncards/2);i++){
         positions.push(Number(i));
@@ -17,11 +18,43 @@ function defineCards(){
     }
     positions.sort(comparador);//randomiza
 }
-
+//usado para misturar as cartas
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-
+//
+function createCards(){
+    for(let i=0;i<(ncards-1);i++){
+        document.querySelector(".cards").innerHTML+=`<div class="card" onclick="flip(this)">
+        <div class="front-face face">
+            <img src="media/front.png" alt="Parrot">
+        </div>
+        <div class="back-face face">
+            <img src="media/front.png" alt="Parrot">
+            Verso
+        </div>
+    </div>`
+        // if(positions[i]==0) createCard(bobrossparrot);
+        // if(positions[i]==1) createCard(explodyparrot);
+        // if(positions[i]==2) createCard(fiestaparrot);
+        // if(positions[i]==3) createCard(metalparrot);
+        // if(positions[i]==4) createCard(revertitparrot);
+        // if(positions[i]==5) createCard(tripletsparrot);
+        // if(positions[i]==6) createCard(unicornparrot);
+    }
+}
+function createCard(parrot){
+    document.querySelector(".cards").innerHTML+=`
+    <div class="card" onclick="flip(this)">
+        <div class="front-face face">
+            <img src="media/front.png" alt="Parrot">
+        </div>
+        <div class="back-face face">
+            <img src="media/${parrot}.gif" alt="Parrot">
+            Verso
+        </div>
+    </div>`;
+}
 function flip(element){
 
 }
