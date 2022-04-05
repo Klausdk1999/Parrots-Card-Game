6d1f4c62f16b015ef17a8afeccacac38da3c37b1
KlausDk <klausdk1999@gmail.com>
2022-04-05 21:54:32 +0000

    Array com numero escolhido e misturando

--- Documents/Driven/Projeto_04_parrotsCardGame/script.js
+++ Documents/Driven/Projeto_04_parrotsCardGame/script.js
@@ -1,15 +1,22 @@
-let ncards;
-nCards();
+let ncards=0;
+let positions=[];//criar array para cartas que serao embaralhadas
 
+nCards();
+defineCards();
+//gera o prompt ate que se obtenha valor valido
 function nCards(){
     while(ncards!=4 && ncards!=6 && ncards!=8 && ncards!=10 && ncards!=12 && ncards!=14){
-        ncards=prompt("Com quantas cartas deseja jogar? (4, 6, 8, 10, 12, 14)");
+        ncards=Number(prompt("Com quantas cartas deseja jogar? (4, 6, 8, 10, 12, 14)"));
     }
 }
 
-let positions;
-
-minhaArray.sort(comparador);//randomiza
+function defineCards(){
+    for(let i=0;i<(ncards/2);i++){
+        positions.push(Number(i));
+        positions.push(Number(i));
+    }
+    positions.sort(comparador);//randomiza
+}
 
 function comparador() { 
 	return Math.random() - 0.5; 
